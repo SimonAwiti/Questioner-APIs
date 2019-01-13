794ba4026d29e8bb23a08ee2dd5058c6acb4e54c
SimonAwiti <awitisimon23@gmail.com>
2019-01-14 01:05:05 +0300

    [chore #163177951] removed validations to validation file

--- app/API/version1/users/models.py
+++ app/API/version1/users/models.py
@@ -34,19 +34,10 @@ class Users():
         password = request.json.get('password', None)
         confirm = request.json.get('confirm', None)
 
-        if firstname == '' or lastname == ''  or email == ''  or password == '' or confirm == '':
-            return {'error': 'Fields cannot be empty'}, 401
-
-        if password != confirm:
-            return {'msg':"Passwords do not match"}, 401
-
-        if len(password) < 6 or len(password) > 12:
-            return {'msg': "Password length should be between 6 and 12 characters long"}, 401
 
         duplicate = check_if_user_exists(email)
         if duplicate:
             return {'msg':'User already exists'}, 401
-        
         user_dict = {
             "user_id": len(users) + 1,
             "firstname" : firstname.rstrip(),
@@ -69,7 +60,7 @@ class Users():
 
         credentials = verify_credentials(email, password)
         if not credentials:
-            return {'msg':'Error logging in, ensure email or password are correct'}, 401
+            return {'msg':'Error logging in, ensure email or password are correct or you are registered'}, 401
         
         user = [user for user in users if user['email'] == email.rstrip()]
         access_token = create_access_token(identity={'email':user[0]['email'], 'user_id':user[0]['user_id'], 'admin':user[0]['admin']})
