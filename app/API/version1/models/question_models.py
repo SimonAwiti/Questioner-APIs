c84f875246b4e79bf1c9e60a6fb6a478ed81a738
SimonAwiti <awitisimon23@gmail.com>
2019-01-13 03:44:39 +0300

    [feature #163075650] restructured the codes

--- app/API/version1/models/question_models.py
+++ app/API/version1/models/question_models.py
@@ -3,40 +3,41 @@
 from flask import request, jsonify
 
 from datetime import datetime, timedelta
+from app.API.version1.models.model_meetups import Meetups
 
-
-# List to hold all the questions to be posted
 questions = []
 
 
 class Question():
     """Class to handle the question operation """
-    def create_question(self, body, title, meetup, createdBy):
+    def create_question(self, body, title, meetup_id, createdBy):
         """Method to save question records"""
         body = request.json.get('body', None)
         title= request.json.get('title', None)
-        meetup = request.json.get('meetup', None)
+        meetup_id = request.json.get('meetup_id', None)
         createdBy = request.json.get('createdBy', None)
         
-        # Add all attributes to a meetup dictionary
+        #meetup = Meetups.fetch_one(meetup_id)
+        #if not meetup:
+            #return {
+                #"Error": "Meetup does not exist",
+                #"status": 404
+            #}, 404
+
         question_dict={
             "id": len(questions) + 1,
             "createdOn" : str(datetime.now()),
             "body": body,
             "title": title,
             "votes": 0,
-            "meetup": meetup,
+            "meetup_id": meetup_id,
             "createdBy": createdBy
         }
-        # Append to the meetup list
         questions.append(question_dict)
         return {"msg": "Question succesfully posted"}, 201
 
     def get_all_questions(self):
         """Fetch all question records from the questions list"""
-        # If question list is empty
-        if len(questions) == 0:
-            return {'msg':'No question added yet'}, 404
         return {'All posted questions':questions}, 200
     
     def get_one_question(self, question_id):
@@ -44,7 +45,5 @@ class Question():
         question = [question for question in questions if question['id'] == question_id]
         if question:
             return {'question record': question[0]}, 200
-        # no question found
         return {'msg':'Question record with that ID not found'}, 404
-        
-
+   
