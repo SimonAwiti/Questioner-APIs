2b976f36b66da8212213fbcda7789f539e7c3762
SimonAwiti <awitisimon23@gmail.com>
2019-01-15 17:41:25 +0300

    [chore #163075880] added move test to improve coverage

--- tests/version1/test_users.py
+++ tests/version1/test_users.py
@@ -18,16 +18,61 @@ class TestUsers(unittest.TestCase):
             "password":"pass123",
             "confirm":"pass123",
         }
-
+        self.user2 = {
+            "firstname":"",
+            "lastname":"",
+            "email":"",
+            "password":"",
+            "confirm":"",
+        }
+        self.user3 = {
+            "firstname":"John",
+            "lastname":"Kariuki",
+            "email":"trononawiti@gmail.com",
+            "password":"pa23",
+            "confirm":"pa23",
+        }
+        self.user4 = {
+            "firstname":"Simon",
+            "lastname":"Awiti",
+            "email":"johnawiti@gmail.com",
+            "password":"pass123",
+            "confirm":"conpass123",
+        }
+        self.user5 = {
+            "firstname":"Victor",
+            "lastname":"Awiti",
+            "email":"johnawuvigmail.com",
+            "password":"conpass123",
+            "confirm":"conpass123",
+        }
+        self.user6 = {
+            "firstname":"1",
+            "lastname":"2",
+            "email":"simonawitti@gmail.com",
+            "password":"pass123",
+            "confirm":"pass123",
+        }
+        self.user7 = {
+            "firstname":"Simon",
+            "lastname":"Awiti",
+            "email":"simonawiti@gmail.com",
+            "password":"pass123",
+            "confirm":"pass123",
+        }
         self.login = {
             "email":"simonawiti@gmail.com",
             "password":"pass123"
         }
 
+        self.login2 = {
+            "email":"simonawiti@gmail.com",
+            "password":"pass3"
+        }
     def test_add_user(self):
         """Tests for adding a new user"""
         response = self.client().post(
-            '/api/v1/users/register', 
+            '/api/v1/users/auth/register', 
             data=json.dumps(self.user), 
             content_type='application/json'
             )
@@ -37,9 +82,78 @@ class TestUsers(unittest.TestCase):
     def test_for_successful_login(self):
         """Tests if a user successfully logged in"""
         response = self.client().post(
-            '/api/v1/users/login', 
+            '/api/v1/users/auth/login', 
             data=json.dumps(self.login), 
             content_type='application/json'
             )
         self.assertEqual(response.status_code, 200)
         self.assertIn("User succesfully Logged in", str(response.data))
+
+    def test_add_user_wrong_passwords(self):
+        """Tests for checking if password match"""
+        response = self.client().post(
+            '/api/v1/users/auth/login',
+            data=json.dumps(self.login2), 
+            content_type='application/json')
+        self.assertEqual(response.status_code, 401)
+        self.assertIn("logging in, ensure email or password are correct or you are registered", str(response.data))
+
+    def test_add_user_with_no_fields(self):
+        """Tests for adding a new user with no fields"""
+        response = self.client().post(
+            '/api/v1/users/auth/register', 
+            data=json.dumps(self.user2), 
+            content_type='application/json'
+            )
+        self.assertEqual(response.status_code, 401)
+        self.assertIn("Fields cannot be left empty", str(response.data))
+
+    def test_add_user_with_weak_password(self):
+        """Tests for adding a new user with weak passwords"""
+        response = self.client().post(
+            '/api/v1/users/auth/register', 
+            data=json.dumps(self.user3), 
+            content_type='application/json'
+            )
+        self.assertEqual(response.status_code, 401)
+        self.assertIn("Password should have atleast 5 characters", str(response.data))
+
+    def test_add_user_with_unmatching_passwords(self):
+        """Tests for adding a new user with unmatching password"""
+        response = self.client().post(
+            '/api/v1/users/auth/register', 
+            data=json.dumps(self.user4), 
+            content_type='application/json'
+            )
+        self.assertEqual(response.status_code, 401)
+        self.assertIn("Passwords do not match", str(response.data))
+
+    def test_add_user_with_poor_email(self):
+        """Tests for adding a new user with poor email"""
+        response = self.client().post(
+            '/api/v1/users/auth/register', 
+            data=json.dumps(self.user5), 
+            content_type='application/json'
+            )
+        self.assertEqual(response.status_code, 401)
+        self.assertIn("Invalid email provided", str(response.data))
+    
+    def test_add_user_with_digit_names(self):
+        """Tests for adding a new user with digit"""
+        response = self.client().post(
+            '/api/v1/users/auth/register', 
+            data=json.dumps(self.user6), 
+            content_type='application/json'
+            )
+        self.assertEqual(response.status_code, 401)
+        self.assertIn("User names cannot be digits", str(response.data))
+
+    def test_add_user_who_exists(self):
+        """Tests for adding a new user who exists"""
+        response = self.client().post(
+            '/api/v1/users/auth/register', 
+            data=json.dumps(self.user7), 
+            content_type='application/json'
+            )
+        self.assertEqual(response.status_code, 401)
+        self.assertIn("User already exists", str(response.data))
