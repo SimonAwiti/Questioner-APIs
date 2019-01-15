ea0bd4a98036301dc66f8601ba58b3a6301c67e3
SimonAwiti <awitisimon23@gmail.com>
2019-01-15 18:17:03 +0300

    [chore #163075880] modified configurations  file

--- instance/config.py
+++ instance/config.py
@@ -4,7 +4,6 @@ import os
 class Config(object):
     """Parent configuration class."""
     DEBUG = False
-    CSRF_ENABLED = True
     SECRET = os.getenv('SECRET')
 
 class DevelopmentConfig(Config):
