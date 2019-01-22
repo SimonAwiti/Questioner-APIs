29654b8301491cfdf91227bb41adb8eb36ec0a1e
SimonAwiti <awitisimon23@gmail.com>
2019-01-23 02:31:47 +0300

    [feature #163238802] created validations for the app

--- app/API/utilities/validator.py
+++ app/API/utilities/validator.py
@@ -42,3 +42,21 @@ def validate_questions(args):
             return "valid"
     except Exception as error:
         return "please provide all the fields, missing " + str(error)
+
+def validate_comments(args):
+    """validate comments details"""
+    try:
+        if args["question_id"] == '' or \
+           args["title"] == '' or \
+           args["comment"] == '' :
+            return {'error': 'Fields cannot be left empty'}, 401
+        elif(args["title"]. isdigit()) or \
+            (args["comment"]. isdigit()):
+                return {"error":"The fields should be described in words"},401
+        elif(args["question_id"]. isalpha()):
+            return {"error":"The field should be an integer"},401
+        else:
+            return "valid"
+    except Exception as error:
+        return "please provide all the fields, missing " + str(error)
+
