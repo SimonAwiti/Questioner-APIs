b596859d374c5227364eda6e58855eab37f306ec
SimonAwiti <awitisimon23@gmail.com>
2019-01-23 23:20:43 +0300

    [chore #163422176] modified the return responses for comments

--- app/API/version2/comments/models.py
+++ app/API/version2/comments/models.py
@@ -9,6 +9,15 @@ from app.API.version2.meetups.models import Helper
 
 class Comments:
     """A class that handles all the comments operations"""
+    @staticmethod
+    def json(data):
+        return dict(id=data[0],
+        user_id=data[2],
+        question_id=data[1],
+        title=data[3],
+        comment=data[4],
+        createdOn=data[5]
+        )
 
     def create_comment(self, user_id, question_id, title, comment):
         """Method to create a comment"""
@@ -25,17 +34,20 @@ class Comments:
                                 question_id,\
                                 title,\
                                 comment) \
-                        VALUES ('" + user_id +"', '" + question_id +"', '" + title +"', '" + comment +"')"
+                        VALUES ('" + user_id +"', '" + question_id +"', '" + title +"', '" + comment +"') returning *"
 
             connect = connection.dbconnection()
             cursor = connect.cursor()
             cursor.execute(add_comment, data)
             connect.commit()
+            comment = cursor.fetchone()
              
             response = jsonify({'status': 201,
-                                "msg":'Comment Successfully Posted'})
+                                "msg":'Comment Successfully Posted',
+                                "data": Comments.json(comment) })
             return response
         except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
             response = jsonify({'status': 500,
                                 'error':'A database error occured'})
             response.status_code = 500
@@ -50,6 +62,6 @@ class Comments:
         return make_response(jsonify({
                 "status": 200,
                 "msg": "All posted comments",
-                "data": [comments]
+                "data": [Comments.json(comment) for comment in comments]
                 }))
                 
