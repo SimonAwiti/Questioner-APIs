365be7c3e6dea9a014ad751c868767a91ed95e72
SimonAwiti <awitisimon23@gmail.com>
2019-01-23 16:53:32 +0300

    [chore #163422176] changed all validation responses on validators

--- app/API/utilities/validator.py
+++ app/API/utilities/validator.py
@@ -2,13 +2,23 @@ def validate_meetups(args):
     """validate meetup details"""
     try:
         if args["location"] == '' or args["topic"] == '':
-            return {'error': 'Fields cannot be left empty'}, 401
+            return{
+                "status": 401,
+                "error": "Fields cannot be left empty"
+                }, 401
         elif( args["location"]. isdigit()) or ( args["topic"]. isdigit()) :
-            return {"error":"The fields should be described in words"},401
+            return{
+                "status": 401,
+                "error": "The fields should be described in words"
+                }, 401
         else:
             return "valid"
     except Exception as error:
-        return "please provide all the fields, missing " + str(error)
+        return{
+                    "status": 401,
+                    "error": "please provide all the fields, missing " + str(error)
+                    }, 401
+
 
 def validate_rsvps(args):
     """validate rsvp details"""
@@ -16,17 +26,32 @@ def validate_rsvps(args):
         if args["response"] == '' or \
            args["user_id"] == '' or \
            args["meetup_id"] == '':
-            return {'error': 'Fields cannot be left empty'}, 401
+           return{
+                "status": 401,
+                "error": "Fields cannot be left empty"
+                }, 401
         elif (args["response"]. isdigit()):
-                return {"error":"The fields should be described in words"},401
+            return{
+                "status": 401,
+                "error": "The fields should be described in words"
+                }, 401
         elif(args["meetup_id"]. isalpha()) or (args["user_id"]. isalpha()):
-            return {"error":"The fields requiring id should be integers"},401
+            return{
+                "status": 401,
+                "error": "The fields requiring id should be integers"
+                }, 401
         elif (args["response"] != "yes"):
-            return {"error":"The response should either be yes, no or maybe..."},401
+            return{
+                "status": 401,
+                "error": "The response should either be yes, no or maybe..."
+                }, 401
         else:
             return "valid"
     except Exception as error:
-        return "please provide all the fields, missing " + str(error)
+        return{
+                    "status": 401,
+                    "error": "please provide all the fields, missing " + str(error)
+                    }, 401
 
 def validate_questions(args):
     """validate question details"""
@@ -34,16 +59,29 @@ def validate_questions(args):
         if args["body"] == '' or \
            args["title"] == '' or \
            args["meetup_id"] == '' :
-            return {'error': 'Fields cannot be left empty'}, 401
+           return{
+                "status": 401,
+                "error": "Fields cannot be left empty"
+                }, 401
         elif(args["body"]. isdigit()) or \
             (args["title"]. isdigit()):
-                return {"error":"The fields should be described in words"},401
+            return{
+                "status": 401,
+                "error": "The fields should be described in words"
+                }, 401
         elif(args["meetup_id"]. isalpha()):
-            return {"error":"The field should be an integer"},401
+            return{
+                "status": 401,
+                "error": "The field should be an intege"
+                }, 401
         else:
             return "valid"
     except Exception as error:
-        return "please provide all the fields, missing " + str(error)
+        return{
+                    "status": 401,
+                    "error": "please provide all the fields, missing " + str(error)
+                    }, 401
+
 
 def validate_comments(args):
     """validate comments details"""
@@ -51,14 +89,27 @@ def validate_comments(args):
         if args["question_id"] == '' or \
            args["title"] == '' or \
            args["comment"] == '' :
-            return {'error': 'Fields cannot be left empty'}, 401
+           return{
+                "status": 401,
+                "error": "Fields cannot be left empty"
+                }, 401
         elif(args["title"]. isdigit()) or \
             (args["comment"]. isdigit()):
-                return {"error":"The fields should be described in words"},401
+            return{
+                "status": 401,
+                "error": "The fields should be described in words"
+                }, 401
         elif(args["question_id"]. isalpha()):
-            return {"error":"The field should be an integer"},401
+            return{
+                "status": 401,
+                "error": "The field should be an integer"
+                }, 401
         else:
             return "valid"
     except Exception as error:
-        return "please provide all the fields, missing " + str(error)
+        return{
+                    "status": 401,
+                    "error": "please provide all the fields, missing " + str(error)
+                    }, 401
+                    
 
