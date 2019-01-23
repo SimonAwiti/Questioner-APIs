65a0a49bff3f7cfef6eec13f996de40b407da8ff
SimonAwiti <awitisimon23@gmail.com>
2019-01-23 23:38:25 +0300

    [chore #163422176] updated rsvps response

--- app/API/version2/rsvps/models.py
+++ app/API/version2/rsvps/models.py
@@ -10,6 +10,14 @@ from app.API.version2.meetups.models import Helper
 class Rsvps:
     """A class that handles all the comments operations"""
 
+    @staticmethod
+    def json(data):
+        return dict(id=data[0],
+        user_id=data[1],
+        meetup_id=data[2],
+        response=data[3]
+        )
+
     def create_rsvp(self, user_id, meetup_id, response):
         """Method to create an rsvp"""
         data = {
@@ -21,21 +29,25 @@ class Rsvps:
             add_rsvp = "INSERT INTO \
                         rsvps (\
                                 user_id,\
-                                comment_id,\
+                                meetup_id,\
                                 response) \
-                        VALUES ('" + user_id +"', '" + meetup_id +"', '" + response +"')"
+                        VALUES ('" + user_id +"', '" + meetup_id +"', '" + response +"') returning *"
 
             connect = connection.dbconnection()
             cursor = connect.cursor()
             cursor.execute(add_rsvp, data)
             connect.commit()
+            rsvps = cursor.fetchone()
              
             resp = jsonify({'status': 201,
-                                "msg":'RSVP Successfully Posted'})
-            return resp
-            resp.status_code = 201
+                                "msg":'RSVP Successfully Posted',
+                                "data": Rsvps.json(rsvps)})
+            return response
+            response.status_code = 201
 
         except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
             resp = jsonify({'status': 500,
                                 'error':'A database error occured'})
             resp.status_code = 500
+        return resp
